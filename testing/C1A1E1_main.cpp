3d5aecd7cb4831be5d7a00562f80e45f5bd16e00
pauldefusco <pauldefusco@cloudera.com>
2022-01-17 20:30:48 -0800

    Modified formatting and added new line char at end of last comment

--- testing/C1A1E1_main.cpp
+++ testing/C1A1E1_main.cpp
@@ -8,13 +8,11 @@
 #include "C1A1E1_main.hpp"
 #include <iostream>
 
-int main() {
+int main(void) {
     int var;
-    
     std::cout << "Please enter any decimal integer numeric value\n";
     std::cin >> var;
-    std::cout << std::dec << var << " decimal = " << std::oct << var << " octal = " << std::hex << var << " hexadeximal\n";
-    
-    
+    std::cout << std::dec << var << " decimal = " << std::oct << var
+    << " octal = " << std::hex << var << " hexadeximal\n";
     return 0;
 }
